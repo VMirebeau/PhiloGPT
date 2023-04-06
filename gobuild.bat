2dc1eddbc2fc0163c12cc5e63deb9625e32237bd
V. Mirebeau <vivien.mirebeau+github@gmail.com>
2023-04-06 20:28:55 +0200

    Dernières modifications avant de passer des valeurs par contexte React

---
+++ gobuild.bat
@@ -0,0 +1,3 @@
+md "D:\Programmation\chathub3\dist\assets\avatars"
+copy "D:\Programmation\PhiloGPT\pics\avatars" "D:\Programmation\chathub3\dist\assets\avatars"
+copy "D:\Programmation\chathub3\src\assets\icon.png" "D:\Programmation\chathub3\dist\assets\"
