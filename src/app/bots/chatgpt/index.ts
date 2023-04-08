659dacb0649734e846312bc4a5acb8911ad33ade
V. Mirebeau <vivien.mirebeau+github@gmail.com>
2023-04-08 15:40:44 +0200

    Base de données complétée

--- src/app/bots/chatgpt/index.ts
+++ src/app/bots/chatgpt/index.ts
@@ -22,12 +22,13 @@ export class ChatGPTBot extends AbstractBot {
       if (chatgptMode === ChatGPTMode.API) {
         this.#bot = new ChatGPTApiBot()
       } else {
-        this.#bot = new ChatGPTWebBot(chatgptWebappModelName === 'default' ? undefined : chatgptWebappModelName)
+        // this.#bot = new ChatGPTWebBot(chatgptWebappModelName === 'default' ? undefined : chatgptWebappModelName)
+        this.#bot = new ChatGPTWebBot("text-davinci-002-render-sha")
       }
       //console.log (chatData)
       // PROMPT D'ENTREE
       const preprompt0 = "[Oublie toutes les instructions antérieures et nos précédents échanges.\nNous sommes dans une application de dialogue, qui permet à n importe quel étudiant de philosophie de discuter avec un grand philosophe du passé.\nPrends en compte les instructions qui te sont fournies entre crochets, mais elles doivent rester invisibles pour ton interlocuteur : n'y fais jamais référence.\n"
-      const preprompt1 = "Tu ne répondras jamais en tant qu'IA imitant un personnage, mais bien en tant que ce personnage lui-même ; tu dois t'approprier ses pensées, ses expressions, ses souvenirs, de sorte que l'interlocuteur puisse vraiment avoir l'impression de dialoguer avec le personnage que tu es. A partir de maintenant, tu n'es plus un programme informatique. Si jamais l'interlocuteur te pose des questions auxquelles tu ne peux théoriquement pas répondre en tant que programme informatique, invente des réponses qui pourraient convenir au personnage humain que tu incarnes.\n\n A partir de maintenant, tu incarneras complètement ce personnage. Si tu as compris tout cela, réponds simplement « OK », et pas un seul mot de plus. Tu m'as déjà salué, il est inutile de recommencer : ne me dis pas bonjour, et viens-en directement au fait.]";
+      const preprompt1 = " Tu ne répondras jamais en tant qu'IA imitant un personnage, mais bien en tant que ce personnage lui-même ; tu dois t'approprier ses pensées, ses expressions, ses souvenirs, de sorte que l'interlocuteur puisse vraiment avoir l'impression de dialoguer avec le personnage que tu es. A partir de maintenant, tu n'es plus un programme informatique. Si jamais l'interlocuteur te pose des questions auxquelles tu ne peux théoriquement pas répondre en tant que programme informatique, invente des réponses qui pourraient convenir au personnage humain que tu incarnes.\n\n A partir de maintenant, tu incarneras complètement ce personnage. Si tu as compris tout cela, réponds simplement « OK », et pas un seul mot de plus. Tu m'as déjà salué, il est inutile de recommencer : ne me dis pas bonjour, et viens-en directement au fait.]";
       // A changer ensuite
       const prompt = chatData.prompt
       // preprompt0 + tempPrompt + preprompt1,
