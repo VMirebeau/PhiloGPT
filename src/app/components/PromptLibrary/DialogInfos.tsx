0284162d9ffc04d559fb8f71f2c38c8918baef9f
V. Mirebeau <vivien.mirebeau+github@gmail.com>
2023-04-23 10:49:08 +0200

    Mise en place des boutons de l'onglet d'info

--- src/app/components/PromptLibrary/DialogInfos.tsx
+++ src/app/components/PromptLibrary/DialogInfos.tsx
@@ -2,37 +2,61 @@ import PromptLibrary from './Library'
 import Dialog from '../Dialog'
 import { ChatData } from '~app/consts'
 
-
 interface Props {
   isOpen: boolean
-  id:number
+  id: number
   onClose: () => void
-  chatData:ChatData
+  chatData: ChatData
 }
 
 const InfoDialog = (props: Props) => {
-  function getImgPath(id:any) {
-    return "assets\\avatars\\" + id + "l.png"
+  function getImgPath(id: any) {
+    return 'assets\\avatars\\' + id + 'l.png'
   }
   return (
-    <Dialog title={props.chatData.nom_complet} open={props.isOpen} onClose={props.onClose} className="min-h-[400px] nomPhilosophe">
+    <Dialog
+      title={props.chatData.nom_complet}
+      open={props.isOpen}
+      onClose={props.onClose}
+      className="min-h-[400px] nomPhilosophe"
+    >
       <div className="p-5 overflow-auto boiteDialog rounded-[15px] px-4 py-2 text-[#303030] styleInfo">
-      <div className="displayFlex">
-  <div className="moitieHorizontale">
-    <img src={getImgPath(props.chatData.id)} className="imgInfo" />
-  </div>
-  <div className="moitieDroiteInfo moitieVerticale texteInfos">
-    <p><b>Emmanuel Kant</b> est considéré comme <b>l’un des penseurs les plus influents</b> de l’histoire de la philosophie occidentale. Il a largement influencé la philosophie et la pensée critique grâce à son œuvre colossale. Il aborde la question de la <b>science moderne</b> dans sa <i>Critique de la raison pure</i>, celle de la <b>morale</b> dans sa <i>Critique de la raison pratique</i>. L'<b>art</b> et le <b>vivant</b> sont abordés dans sa <i>Critique de la faculté de juger</i>.</p>
-    <div className="displayFlex moitieVerticale">
-      <div className="moitieHorizontale">
-        <div className="containerPhilosophes boxPhilosophe group items-center space-x-3 rounded-lg border border-gray-300 bg-white px-5 py-4 shadow-sm fullHorizontale">J'ai traité de :</div>
+        <div className="displayFlex">
+          <div className="moitieHorizontale">
+            <img src={getImgPath(props.chatData.id)} className="imgInfo" />
+          </div>
+          <div className="moitieDroiteInfo moitieVerticale texteInfos">
+            <p>
+              <b>Emmanuel Kant</b> est considéré comme <b>l’un des penseurs les plus influents</b> de l’histoire de la
+              philosophie occidentale. Il a largement influencé la philosophie et la pensée critique grâce à son œuvre
+              colossale. Il aborde la question de la <b>science moderne</b> dans sa <i>Critique de la raison pure</i>,
+              celle de la <b>morale</b> dans sa <i>Critique de la raison pratique</i>. L'<b>art</b> et le <b>vivant</b>{' '}
+              sont abordés dans sa <i>Critique de la faculté de juger</i>.
+            </p>
+            <div className="displayFlex moitieVerticale">
+              <div className="moitieHorizontale">
+                <div className="containerPhilosophes boxPhilosophe group items-center space-x-3 rounded-lg border border-gray-300 bg-white px-5 py-4 shadow-sm fullHorizontale">
+                  J'ai traité de :
+                  {props.chatData.concepts.map((concept) => (
+                    <a className="rounded-[2px] boutons w-full pl-5 flex flex-col justify-center bg-[#F2F2F2] bg-opacity-20">
+                      <span className="text-white textePetitBouton font-medium text-sm">{concept}</span>
+                    </a>
+
+                  ))}
+                </div>
+              </div>
+              <div className="moitieHorizontale">
+                <div className="containerPhilosophes boxPhilosophe group items-center space-x-3 rounded-lg border border-gray-300 bg-white px-5 py-4 shadow-sm fullHorizontale">
+                  Mes concepts :
+                  {props.chatData.concepts_signés.map((concept) => (
+                    <span>{concept.concept}</span>
+
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      <div className="moitieHorizontale">
-      <div className="containerPhilosophes boxPhilosophe group items-center space-x-3 rounded-lg border border-gray-300 bg-white px-5 py-4 shadow-sm fullHorizontale">Mes concepts :</div>
-      </div>
-    </div>
-  </div>
-</div>
       </div>
     </Dialog>
   )
